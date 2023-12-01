09015fdf785eb9d5e6bb0c89479bbe74f4211b57
Lukasz Konopski <konopski@gmail.com>
2023-12-01 16:51:37 +0100

    day one

---
+++ 2023-01/dayone.sh
@@ -0,0 +1,1 @@
+cat input.txt | sed -e "s/one/o1e/g" -e "s/two/t2o/g" -e "s/three/t3e/g" -e "s/four/f4r/g" -e "s/five/f5e/g" -e "s/six/s6x/g" -e "s/seven/s7n/g" -e "s/eight/e8t/g" -e "s/nine/n9e/g"  -e "s/[^0-9]//g" | awk -F "" "NF{ print \$1 \$NF}" | awk "{ s=s+\$1 } END { print s } "
